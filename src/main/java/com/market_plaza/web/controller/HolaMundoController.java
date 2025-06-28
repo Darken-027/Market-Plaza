0aaf57cfa4b580d2ac36cad8cec9e220ee2e1637
Ahstell <torut1638@gmail.com>
2025-06-28 10:30:34 -0500

    Se movio el archivo HolaMundoController al paquete we.controller

--- src/main/java/com/market_plaza/web/controller/HolaMundoController.java
+++ src/main/java/com/market_plaza/web/controller/HolaMundoController.java
@@ -1,4 +1,4 @@
-package com.market_plaza;
+package com.market_plaza.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
