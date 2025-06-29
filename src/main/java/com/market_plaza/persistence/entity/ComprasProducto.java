56538a97f99a1078c02511a8b46763d08c12dc0c
Ahstell <torut1638@gmail.com>
2025-06-29 17:45:50 -0500

    Se crea la clase ComprasProductoPK

--- src/main/java/com/market_plaza/persistence/entity/ComprasProducto.java
+++ src/main/java/com/market_plaza/persistence/entity/ComprasProducto.java
@@ -1,4 +1,10 @@
 package com.market_plaza.persistence.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "compras_productos")
 public class ComprasProducto {
+
 }
---
+++ src/main/java/com/market_plaza/persistence/entity/ComprasProductoPK.java
@@ -0,0 +1,4 @@
+package com.market_plaza.persistence.entity;
+
+public class ComprasProductoPK {
+}
