64e4f03067a9f111c0f12764a8a4a5205851f9bc
Ahstell <torut1638@gmail.com>
2025-06-28 23:57:31 -0500

    Creacion de todos los atributos de la clase

--- src/main/java/com/market_plaza/persistence/entity/Compra.java
+++ src/main/java/com/market_plaza/persistence/entity/Compra.java
@@ -1,5 +1,33 @@
 package com.market_plaza.persistence.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "compras")
 public class Compra {
-     
+
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_cliente")
+    private String idCliente;
+
+
+    private LocalDateTime fecha;
+
+    @Column(name = "medio_pago")
+    private String medioPago;
+
+    private String Comentario;
+    private String estado;
+
+    
 }
+
