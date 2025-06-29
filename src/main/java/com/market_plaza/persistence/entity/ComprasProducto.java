232eba9c19d21ce3d6cc724c23a32f4726b211f1
Ahstell <torut1638@gmail.com>
2025-06-29 17:55:22 -0500

    Creacion de los atribuctos de la clase ComprasProducto

--- src/main/java/com/market_plaza/persistence/entity/ComprasProducto.java
+++ src/main/java/com/market_plaza/persistence/entity/ComprasProducto.java
@@ -1,10 +1,16 @@
 package com.market_plaza.persistence.entity;
 
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "compras_productos")
 public class ComprasProducto {
+    @Embedded
+    private ComprasProductoPK id;
 
+    private Integer cantidad;
+    private Double total;
+    private Boolean estado;
 }
