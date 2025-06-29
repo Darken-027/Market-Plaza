ed05446d8b3718c3c0b5b9ad8ccb10bcc49501cb
Ahstell <torut1638@gmail.com>
2025-06-29 17:52:05 -0500

    Configuracion de la clase compraproductoAK y creacion de constructores

--- src/main/java/com/market_plaza/persistence/entity/ComprasProductoPK.java
+++ src/main/java/com/market_plaza/persistence/entity/ComprasProductoPK.java
@@ -1,4 +1,31 @@
 package com.market_plaza.persistence.entity;
 
-public class ComprasProductoPK {
+import jakarta.persistence.Column;
+
+import java.io.Serializable;
+
+@Embedbable
+public class ComprasProductoPK implements Serializable {
+
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_producto")
+    private Interger idProducto;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Interger getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Interger idProducto) {
+        this.idProducto = idProducto;
+    }
 }
