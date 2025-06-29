22def6f7f3d2f0b95be064fa1168100ed46b434e
Ahstell <torut1638@gmail.com>
2025-06-28 23:43:49 -0500

    Generando los contructores para la clase producto

--- src/main/java/com/market_plaza/persistence/entity/Producto.java
+++ src/main/java/com/market_plaza/persistence/entity/Producto.java
@@ -32,5 +32,61 @@ public class Producto {
     @Column(name = "cantidad_stock")
     private Integer cantidadStock;
 
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public Integer getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(Integer cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public Double getPreioVenta() {
+        return preioVenta;
+    }
+
+    public void setPreioVenta(Double preioVenta) {
+        this.preioVenta = preioVenta;
+    }
+
+    public String getCodigoBarra() {
+        return codigoBarra;
+    }
+
+    public void setCodigoBarra(String codigoBarra) {
+        this.codigoBarra = codigoBarra;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Private getInteger() {
+        return Integer;
+    }
+
+    public void setInteger(Private integer) {
+        Integer = integer;
+    }
+
     private Boolean estado;
 }
